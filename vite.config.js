24865d778123d9b8e7ccda3f51db1f7e6b1b0a4b
Harsh Dubey <dubeyharsh320@gmail.com>
2025-06-10 06:59:07 +0530

    deployment file updated

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: 3000,
+    host: true,
+  }
 })
